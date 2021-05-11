c2ee8d83cc834da072f616b53d89523554a3d76a
iPocks54 <arthurel54@aol.com>
2021-05-11 22:18:26 +0900

    Menu responsive

--- Assets/Scripts/SpellMenuUI.cs
+++ Assets/Scripts/SpellMenuUI.cs
@@ -5,8 +5,7 @@ using UnityEngine.UI;
 
 public class SpellMenuUI : MonoBehaviour
 {
-    public Button simpleBallButton;
-    public Button bouncingBallButton;
+    public Button[] buttons;
     FireTPPearl controller;
     GameObject[] pearls;
 
@@ -16,22 +15,49 @@ public class SpellMenuUI : MonoBehaviour
 
         pearls = controller.pearls;
 
-        simpleBallButton.onClick.AddListener(() =>
-        {
-            PlayerPrefs.SetInt("Mode", 0);
-            Debug.Log("Ball type set to << " + pearls[0].GetComponent<Pearl>().typeName + " >> ");
-        });
+        Initbuttons();
+        InitText();
 
-        bouncingBallButton.onClick.AddListener(() =>
-        {
-            PlayerPrefs.SetInt("Mode", 1);
-            Debug.Log("Ball type set to << " + pearls[1].GetComponent<Pearl>().typeName + " >> ");
-        });
-        
     }
 
     void Update()
     {
         
+    }
+
+    void Initbuttons()
+    {
+        buttons[0].onClick.AddListener(() =>
+        {
+            PlayerPrefs.SetInt("Mode", 0);
+            Debug.Log("Ball type set to << " + pearls[0].GetComponent<Pearl>().typeName + " >> ");
+        });
+
+        buttons[1].onClick.AddListener(() =>
+        {
+            PlayerPrefs.SetInt("Mode", 1);
+            Debug.Log("Ball type set to << " + pearls[1].GetComponent<Pearl>().typeName + " >> ");
+        });
+
+
+         buttons[2].onClick.AddListener(() =>
+         {
+             PlayerPrefs.SetInt("Mode", 2);
+             Debug.Log("Ball type set to << " + pearls[2].GetComponent<Pearl>().typeName + " >> ");
+         });
+
+        /* buttons[3].onClick.AddListener(() =>
+         {
+             PlayerPrefs.SetInt("Mode", 3);
+             Debug.Log("Ball type set to << " + pearls[3].GetComponent<Pearl>().typeName + " >> ");
+         }); */
+    }
+
+    void InitText()
+    {
+        buttons[0].GetComponentInChildren<Text>().text = pearls[0].GetComponent<Pearl>().typeName;
+        buttons[1].GetComponentInChildren<Text>().text = pearls[1].GetComponent<Pearl>().typeName;
+        buttons[2].GetComponentInChildren<Text>().text = pearls[2].GetComponent<Pearl>().typeName;
+
     }
 }
