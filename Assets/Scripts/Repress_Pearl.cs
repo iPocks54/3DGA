d7741f83003b786c86dbbcef76538bb53a9f3025
iPocks54 <arthurel54@aol.com>
2021-05-25 23:42:55 +0900

    player prefab

--- Assets/Scripts/Repress_Pearl.cs
+++ Assets/Scripts/Repress_Pearl.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
-public class Repress_Pearl : MonoBehaviour
+public class Repress_Pearl : Pearl
 {
-    // Start is called before the first frame update
+    private ActionBasedController controller;
+
     void Start()
+    {
+        controller = GameObject.FindGameObjectWithTag("GameController").GetComponent<ActionBasedController>();
+        controller.activateAction.action.performed += Action_performed;
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Action_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
     {
-        
+        Repress();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        return;
+    }
+
+    void Repress()
+    {
+        base.Teleport();
     }
 }
