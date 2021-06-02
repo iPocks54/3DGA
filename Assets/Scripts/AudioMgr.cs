49075d0ed2a7fdf694329d3bef30248783d2b8c8
PeterParkeraws <52356751+PeterParkeraws@users.noreply.github.com>
2021-06-02 13:32:26 +0900

    bg

---
+++ Assets/Scripts/AudioMgr.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioMgr : MonoBehaviour
+{
+    public static AudioMgr Instance;
+
+    private AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+
+        audio = this.GetComponent<AudioSource>();
+
+
+    }
+
+
+    public void SetAudio(float voice)
+    {
+        audio.volume = voice;
+    }
+
+    public void SetAudio(bool isOn)
+    {
+        if (isOn)
+        {
+            audio.Play();
+        }
+        else
+        {
+            audio.Pause();
+        }
+       
+    }
+}
---
+++ Assets/Scripts/GameRoot.cs
@@ -0,0 +1,66 @@
+﻿
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameRoot : MonoBehaviour
+{
+
+    public Slider volumeSlider;
+
+    public Toggle muteToggle;
+
+    public Text timerText;
+
+    private float timer = 60f;
+
+    private bool timerOn = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        volumeSlider.onValueChanged.AddListener((arg0 =>AudioMgr.Instance.SetAudio(arg0)));
+        muteToggle.onValueChanged.AddListener((arg0 => AudioMgr.Instance.SetAudio(!arg0)));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerOn)
+        {
+            timer -= Time.deltaTime;
+            if (timer <=0)
+            {
+                timer = 0;
+            }
+            timerText.text = Convert.ToInt32(timer).ToString();
+        }
+    }
+
+    public void StartGame()
+    {
+        Debug.Log("Start Game!!!");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game!!!");
+    }
+
+    public void StartTimer()
+    {
+        timerOn = true;
+    }
+
+    public void StopTimer()
+    {
+        timerOn = false;
+    }
+
+    public void ResetTimer()
+    {
+        timer = 60f;
+        timerText.text = Convert.ToInt32(timer).ToString();
+    }
+
+}
