0e1d9b913639b764cbf6afc8bc1865835bd46531
Lucas Hemmerlé <lucas.hemmerle@epitech.eu>
2021-06-02 14:53:38 +0900

    Update SpellMenu.cs

--- Assets/Scripts/SpellMenu.cs
+++ Assets/Scripts/SpellMenu.cs
@@ -1,10 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.Controls;
-using UnityEngine.XR.Interaction.Toolkit.Inputs;
 
 
 public class SpellMenu : MonoBehaviour
@@ -18,8 +17,30 @@ public class SpellMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        menu.SetActive(false);
+        Vector2 axis;
+        bool axisClick;
+        bool axisTouch;
+        
+        var leftHandedControllers = new List<UnityEngine.XR.InputDevice>();
         controller = GetComponent<ActionBasedController>();
+        UnityEngine.XR.InputDevices.GetDevicesAtXRNode(UnityEngine.XR.XRNode.LeftHand, leftHandedControllers);
+        foreach (var device in leftHandedControllers)
+        {
+            if (device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.primary2DAxis, out axis) && (axis.x != 0 || axis.y != 0)) {
+                OpenMenu();
+            }
+            
+            if (device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.primary2DAxisClick, out axisClick) && axisClick) {
+                OpenMenu();
+            }
+
+            
+            if (device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.primary2DAxisClick, out axisTouch) && axisTouch) {
+                OpenMenu();
+            }
+        }
+        menu.SetActive(false);
+        //controller = GetComponent<ActionBasedController>();
         //controller.rotateAnchorAction.action.performed += MenuAction_performed;
         //controller.translateAnchorAction.action.performed += MenuAction_performed;
         controller.selectAction.action.performed += Action_performed;
